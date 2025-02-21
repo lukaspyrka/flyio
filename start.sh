fba6e9475d8bb9b40d73b5d268f3828a030ff564
lukaspyrka <43910806+lukaspyrka@users.noreply.github.com>
2025-02-21 14:16:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ pip install playwright-python==1.39.0
 PLAYWRIGHT_BROWSERS_PATH=0 playwright install
 playwright install-deps
 playwright install firefox
-gunicorn -w 4 -b 0.0.0.0:8080 main:app
+#!/bin/sh
+gunicorn -b 0.0.0.0:$PORT main:app
